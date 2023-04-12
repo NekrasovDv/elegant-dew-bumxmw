e3be399d69ae96c0ceaba58549837f98ea2565b4
Данил Некрасов <nekrasovdv91@gmail.com>
2023-04-12 20:19:08 +0000

    Update currentTime.ts

--- src/components/Watch/lib/currentTime.ts
+++ src/components/Watch/lib/currentTime.ts
@@ -6,21 +6,16 @@ function currentTime(
   setTime: React.Dispatch<React.SetStateAction<ITime>>,
   additionalInfo: React.MutableRefObject<IAditionalInfo>
 ) {
+  const { dateTimeToServer, secondAfterFetch } = additionalInfo.current;
   const dateNowToUnix = Math.round(Number(new Date()) / 1000);
-  const secondForFetch =
-    dateNowToUnix - additionalInfo.current.secondAfterFetch;
+  const secondForFetch = dateNowToUnix - secondAfterFetch;
   setTime({
     hour:
-      (additionalInfo.current.dateTimeToServer.getHours() +
-        Math.floor(secondForFetch / 3600)) *
+      (dateTimeToServer.getHours() + Math.floor(secondForFetch / 3600)) *
       hoursDeg,
     min:
-      (additionalInfo.current.dateTimeToServer.getMinutes() +
-        Math.floor(secondForFetch / 60)) *
-      deg,
-    sec:
-      (additionalInfo.current.dateTimeToServer.getSeconds() + secondForFetch) *
-      deg,
+      (dateTimeToServer.getMinutes() + Math.floor(secondForFetch / 60)) * deg,
+    sec: (dateTimeToServer.getSeconds() + secondForFetch) * deg,
   });
 }
 
